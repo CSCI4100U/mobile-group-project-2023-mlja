571f5bef297581238562fe5b9b5442a96951b80d
Ajmain-Khan <ark.ajmain@gmail.com>
2023-12-07 19:09:01 -0500

    Rudamentary workout session page

--- life_balance_plus/lib/view/pages/workout/workout_session_page.dart
+++ life_balance_plus/lib/view/pages/workout/workout_session_page.dart
@@ -1,26 +1,23 @@
 import 'package:flutter/material.dart';
-
-
-class Set {
-  int set;
-  double weight;
-  int reps;
-
-  Set({
-    required this.set,
-    required this.weight,
-    required this.reps,
-  });
-}
+import 'package:life_balance_plus/data/model/exercise.dart';
+import 'package:life_balance_plus/data/model/workout_plan.dart';
 
 class WorkoutSessionPage extends StatefulWidget {
+  final WorkoutPlan? workoutPlan;
+  const WorkoutSessionPage({
+    super.key,
+    this.workoutPlan,
+  });
+
   @override
-  _WorkoutSessionPageState createState() => _WorkoutSessionPageState();
+  State<WorkoutSessionPage> createState() => _WorkoutSessionPageState();
 }
 
 class _WorkoutSessionPageState extends State<WorkoutSessionPage> {
+  final _formKey = GlobalKey<FormState>();
   List<Set> data = [Set(set: 1, weight: 0, reps: 0)];
   int sets = 1;
+  List<Exercise> exercises = [];
 
   @override
   void initState() {
@@ -36,45 +33,61 @@ class _WorkoutSessionPageState extends State<WorkoutSessionPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: data.length < 1 ? Center(child: Text('No items')) :
-      Column(
-        children: [
-          ListView.builder(
-            shrinkWrap: true,
-            itemCount: data.length,
-            itemBuilder: (context, index) {
-              return Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Text('Set ${index + 1}'),
-                  TextField(
-                    onChanged: (value) {
-                      setState(() {
-                        data[index].weight = double.parse(value);
-                      });
-                    },
-                  ),
-                  TextField(
-                    onChanged: (value) {
-                      setState(() {
-                        data[index].reps = int.parse(value);
-                      });
-                    }
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.delete),
-                    onPressed: () {
-                      _deleteRow(index);
-                    },
-                  ),
-                ],
-              );
-            },
+      appBar: AppBar(
+        title: const Text('Upper Split'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.timer),
+            onPressed: () => _showTimerDialog(context),
           ),
         ],
       ),
+      body: data.length < 1
+          ? Center(child: Text('No items'))
+          : Column(
+              children: [
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: data.length,
+                  itemBuilder: (context, index) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text('Set ${index + 1}'),
+                        SizedBox(
+                          width: 50,
+                          child: TextField(
+                            keyboardType: TextInputType.number,
+                            onChanged: (value) {
+                              setState(() {
+                                data[index].weight = double.parse(value);
+                              });
+                            },
+                          ),
+                        ),
+                        SizedBox(
+                          width: 50,
+                          child: TextField(
+                              keyboardType: TextInputType.number,
+                              onChanged: (value) {
+                                setState(() {
+                                  data[index].reps = int.parse(value);
+                                });
+                              }),
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () {
+                            _deleteRow(index);
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                ),
+              ],
+            ),
       floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.green,
         onPressed: _addRow,
         child: Icon(Icons.add),
       ),
@@ -91,7 +104,60 @@ class _WorkoutSessionPageState extends State<WorkoutSessionPage> {
   void _addRow() {
     setState(() {
       sets++;
-      data.add( Set( set: sets, weight: 0, reps: 0));
+      data.add(Set(set: sets, weight: 0, reps: 0));
     });
   }
+
+  Future<void> _showTimerDialog(BuildContext context) async {
+    int selectedTime = 60; // Default time in minutes
+
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Set Timer'),
+          content: Column(
+            children: [
+              Text('Select time in minutes:'),
+              SizedBox(height: 10),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(labelText: 'Minutes'),
+                onChanged: (value) {
+                  selectedTime = int.tryParse(value) ?? selectedTime;
+                },
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                // Add timer functionality here using selectedTime
+                Navigator.of(context).pop();
+              },
+              child: Text('Start'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class Set {
+  int set;
+  double weight;
+  int reps;
+
+  Set({
+    required this.set,
+    required this.weight,
+    required this.reps,
+  });
 }
